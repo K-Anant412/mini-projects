47403d901e2a11d545131ecadd906773a8ae4fa2
K-Anant412 <anantkore412@gmail.com>
2026-07-29 13:28:56 +0530

    creating react environment for UI desing

---
+++ MusicPlayer/Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+export default function App() {
+  return (
+    <>
+      <section className='h-screen w-screen flex items-center justify-center p-20 bg-linear-to-t from-[#403d88ee] via-[#8B639B] to-[#AF719D]'>
+
+        <div className='w-full h-full border-none rounded-3xl bg-[#8B639B] shadow-[8px_8px_20px_rgba(0,0,0,0.3),-8px_-8px_20px_rgba(255,255,255,0.22)]'>
+
+        </div>
+
+      </section>
+    </>
+  );
+}
