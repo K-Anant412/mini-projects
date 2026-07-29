9a416f35d6c2e624de63b446518f99fee6460eed
K-Anant412 <anantkore412@gmail.com>
2026-07-29 18:38:26 +0530

    some changes

--- To-Do_List/Frontend/src/pages/Home.jsx
+++ To-Do_List/Frontend/src/pages/Home.jsx
@@ -33,7 +33,7 @@ const Home = () => {
                   
                   <h1 className='w-[80%] pl-4 font-bold text-gray-700' >Create Task</h1>
                   
-                  <button className='w-[75%] h-20 rounded-2xl border border-amber-50 flex items-center justify-center gap-2 text-2xl font-semibold transition-all duration-20 text-amber-50 hover:text-gray-500 hover:bg-amber-50'>
+                  <button className='w-[75%] h-20 rounded-2xl border border-amber-50 flex items-center justify-center gap-2 text-2xl font-semibold transition-all duration-200 text-amber-50 hover:text-gray-500 hover:bg-amber-50'>
                     <FaPlusCircle className='relative mt-0.5 ' />
                     Add New Task
                   </button>
@@ -63,7 +63,18 @@ const Home = () => {
             {/* Right Section */}
               <span className='w-full h-120 p-4 border-gray-400 rounded-2xl bg-linear-to-b from-[#6367FF] via-[#8494FF] to-[#C9BEFF] shadow-gray-600 shadow-[inset_0_0_8px_2px_rgba(0,0,0,0.06)] mt-10'>
                   <ul className='w-full h-full flex flex-col items-center gap-5 p-4'>
-                    <li className='w-full h-fit p-3 border border-gray-600 rounded-2xl bg-amber-50 text-xl font-semibold'>Making notes</li>
+                    
+                    <li className='flex items-center justify-between w-full h-fit p-3 border border-gray-600 rounded-2xl bg-amber-50 text-xl font-semibold shadow-gray-600 shadow-[3px_3px_12px_2px_rgba(0,0,0,0.06)]'>
+                      Making notes
+
+                      <span className='w-fit h-full flex items-center justify-between max-w-100 gap-4'>
+                        <button className='w-30 h-8 border rounded-sm font-bold transition-all duration-200 hover:bg-gray-500 bg-gray-400 text-white'>shedule</button>
+                        <button className='w-30 h-8 border rounded-sm font-bold transition-all duration-200 hover:bg-orange-500 bg-orange-400 text-white'>details</button>
+                        <button className='w-30 h-8 border rounded-sm font-bold transition-all duration-200 hover:bg-green-500 bg-green-400 text-white'>complete</button>
+              
+                      </span>
+                    </li>
+
                   </ul>
               </span>
           </div>
