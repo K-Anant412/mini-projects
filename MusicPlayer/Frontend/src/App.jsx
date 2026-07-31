babeba204cab65d4b462a1833be6c098e1a7fc47
K-Anant412 <anantkore412@gmail.com>
2026-07-31 21:46:25 +0530

    adding responsive design

--- MusicPlayer/Frontend/src/App.jsx
+++ MusicPlayer/Frontend/src/App.jsx
@@ -41,7 +41,7 @@ export default function App() {
 
   return (
     <>
-      <section className='h-screen w-screen flex items-center justify-center p-10 lg:p-20 bg-linear-to-t from-[#403d88ee] via-[#8B639B] to-[#AF719D]'>
+      <section className='h-screen w-screen flex items-center justify-center p-5 md:p-10 lg:p-20 bg-linear-to-t from-[#403d88ee] via-[#8B639B] to-[#AF719D]'>
 
         <div 
           className='
@@ -50,30 +50,35 @@ export default function App() {
             rounded-3xl 
             bg-[#8B639B]/30 
             flex
+            flex-col md:flex-row
             items-center
             p-5 
-            gap-10
+            gap-6 md:gap-10
             shadow-[8px_8px_20px_rgba(0,0,0,0.3),-8px_-8px_20px_rgba(255,255,255,0.15)]
+            overflow-y-auto md:overflow-visible
         '>
           
-          <span 
+          <div 
             className='
-              w-[40%] h-full
-              border-none 
+              w-full md:w-[40%] 
+              h-[45%] md:h-full
+              border-2 md:border
+              border-none
               rounded-3xl
               shadow-[6px_8px_20px_rgba(0,0,0,0.22),-8px_-8px_20px_rgba(255,255,255,0.12)]
               p-1 flex
               flex-col
               items-center
-              gap-5 px-5
+              gap-3 md:gap-5 px-5
+              shrink-0 md:shrink
           '>
 
             <h1 
               className='
                 w-full h-fit 
-                text-2xl 
+                text-xl md:text-2xl 
                 font-semibold
-                pl-8 pt-3
+                pl-4 md:pl-8 pt-3
                 text-gray-700
                 font-serif
                 shrink-0
@@ -84,47 +89,54 @@ export default function App() {
             <ul 
               className='
                   w-full h-full
-                  border-none mb-5 
+                  border-none mb-3 md:mb-5 
                   flex flex-col
                   items-center
                   justify-center
                   overflow-x-hidden
                   overflow-y-auto
                   scrollbar-none
-                  gap-3 pt-4
+                  gap-3 pt-2 md:pt-4
                   text-white
             '>
               {songs.map(
                 (song) => (
                   <li key={song.id} className='w-full h-13 border rounded-2xl shrink-0 flex items-center gap-5 p-1 px-3'>
+                    
                     <div className='h-full w-11 border rounded-[50%] shrink-0'></div>
-                    <h1 className='text-2xl font-semibold w-full'>{song.title}</h1>
+                    
+                    <h1 className='text-lg md:text-xl font-semibold w-full truncate'>{song.title}</h1>
+                    
                     <button className='h-full w-11 shrink-0 flex items-center justify-center'>
                       <CiHeart size={30}  />
                     </button>
+
                   </li>
                 )
               )}
             </ul>
 
-          </span>
+          </div>
 
           <span
             className='
-              w-[55%] h-full
+              border md:border-2
+              w-full h-[50%]
+              md:w-[55%] md:h-full
               border-none 
               rounded-3xl
               shadow-[6px_8px_20px_rgba(0,0,0,0.22),-8px_-8px_20px_rgba(255,255,255,0.12)]
               p-1 flex
               flex-col
               items-center
-              gap-5 px-5
+              gap-3 md:gap-5 px-5
+              shrink-0 md:shrink
           '>
-            <div className='w-70 h-70 border rounded-[50%] mt-10 bg-gray-400 border-gray-400'>
+            <div className='w-36 h-36 md:w-70 md:h-70 border rounded-[50%] md:mt-10 bg-gray-400 border-gray-400 shrink-0'>
 
             </div>
 
-            <div className='w-[85%] h-12 border rounded-4xl'>
+            <div className='w-[85%] h-12 border text-white rounded-4xl shrink-0'>
               <div className="w-full max-w-xl mx-auto p-4">
                 <input
                   type="range"
@@ -133,33 +145,34 @@ export default function App() {
                   className="w-full cursor-pointer"
                 />
 
-                <p className="mt-2 text-center">not</p>
+                <p className="mt-1 text-center text-xs md:text-sm">not</p>
               </div>
             </div>
 
-            <div className='w-full h-30 flex items-center justify-around text-white'>
+            <div className='w-full h-20 md:h-30 flex items-center justify-around text-white mt-2 md:mt-0 shrink-0'>
 
-              <button className='w-18 h-18 rounded-[50%] flex items-center justify-center'>
-                <RxLoop size={50} />
+              <button className='w-12 h-12 md:h-18 md:w-18 rounded-[50%] flex items-center justify-center'>
+                <RxLoop className='text-[30px] md:text-[50px]' />
               </button>
 
-              <span className='w-[50%] h-full flex items-center justify-around'>
+              <div className='w-[60%] md:w-[50%] h-full flex items-center justify-around'>
 
-                <button className='w-20 h-20 rounded-[50%] flex items-center justify-center'>
-                  <PiFastForwardThin size={55} className=' transform rotate-180' />
+                <button className='w-14 h-14 md:w-20 md:h-20 rounded-[50%] flex items-center justify-center'>
+                  <PiFastForwardThin className=' transform rotate-180 text-[35px] md:text-[55px]' />
                 </button>
 
-                <button className='w-20 h-20 rounded-[50%] flex items-center justify-center'>
-                  <CiPause1 size={50} />
+                <button className='w-14 h-14 md:w-20 md:h-20 rounded-[50%] flex items-center justify-center' onClick={()=>console.log("song played")
+                }>
+                  <CiPause1 className='text-[30px] md:text-[50px]' />
                 </button>
 
-                <button className='w-20 h-20 rounded-[50%] flex items-center justify-center'>
-                  <PiFastForwardThin size={55} />
+                <button className='w-14 h-14 md:w-20 md:h-20 rounded-[50%] flex items-center justify-center'>
+                  <PiFastForwardThin className='text-[35px] md:text-[55px]' />
                 </button>
-              </span>
+              </div>
 
-              <button className='w-18 h-18 rounded-[50%] flex items-center justify-center'>
-                <IoMenu size={50} />
+              <button className='w-12 h-12 md:w-18 md:h-18 rounded-[50%] flex items-center justify-center'>
+                <IoMenu className='text-[30px] md:text-[50px]' />
               </button>
             
             </div>
