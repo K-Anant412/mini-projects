db5fb05cf54114566d7e606cc77d8a5a04c4c82a
K-Anant412 <anantkore412@gmail.com>
2026-08-01 18:13:44 +0530

    completing the UI for homepage

--- To-Do_List/Frontend/src/pages/Home.jsx
+++ To-Do_List/Frontend/src/pages/Home.jsx
@@ -7,13 +7,36 @@ import profile1 from '../assets/profile1.jpg'
 
 const Home = () => {
 
-  const [tasks, setTasks] = useState([])
+  // const [tasks, setTasks] = useState([])
   const [loading, setLoading] = useState(false)
 
   useEffect(() => {
-    
+      const response = async() =>{
+        setLoading(true);
+
+        try {
+          console.log(response.data.data);
+          
+        } catch (error) {
+          console.log(error);
+          
+        }
+        
+      }
+
   }, [])
   
+  const tasks = [
+    "making notes",
+    "cooking food",
+    "reading book",
+    "preparing for exams",
+    "mathematics",
+    "computer networks",
+    "DBMS",
+    "statistics",
+    "environmental science"
+  ]
 
   return(
     <>
@@ -50,10 +73,33 @@ const Home = () => {
           </div>
           {/* right side task panel */}
           <div className="w-full h-full border-none shadow-gray-400 shadow-[inset_0_0_8px_2px_rgba(0,0,0,0.06)] p-10">
+            {/* main dashboard */}
+            <ul className="border flex overflow-x-hidden overflow-y-auto scrollbar-none flex-col gap-8 p-10 w-full h-full rounded-3xl shadow-[8px_8px_20px_rgba(0,0,0,0.3),-8px_-8px_20px_rgba(255,255,255,0.15)] border-none bg-white/20">
+              
+              {/* task list */}
+              {tasks.map(
+                (task, index) => {
+                  return(
+                    <li key={index} className="shrink-0 border w-full h-18 flex items-center p-2 justify-end rounded-3xl border-none shadow-gray-600 shadow-[0_0_6px_2px_rgba(0,0,0,0.08)]">
+                      {/* title */}
+                      <h1 className="w-full h-full flex items-center truncate text-2xl px-5 font-semibold">{task}</h1>
+                      {/* buttons for control */}
+                      <div className="w-fit h-full flex items-center justify-center gap-4 mx-3">
 
-            <div className="border w-full h-full rounded-3xl shadow-[8px_8px_20px_rgba(0,0,0,0.3),-8px_-8px_20px_rgba(255,255,255,0.15)] border-none bg-white/20">
+                        <button className="border w-26 h-10 text-xl font-medium rounded-2xl flex items-center justify-center pb-1 transition-all duration-200 bg-amber-400/40 hover:bg-amber-600/40  text-gray-700 hover:text-gray-100">
+                          details
+                        </button>
+                        <button className="border w-26 h-10 text-xl font-medium rounded-2xl flex items-center justify-center pb-1 transition-all duration-200 bg-green-400/40 hover:bg-green-600/40  text-gray-700 hover:text-gray-100">
+                          complete
+                        </button>
 
-            </div>
+                      </div>
+                    </li>
+                  )
+                }
+              )}
+
+            </ul>
           </div>
         </section>
     </>
