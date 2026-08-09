40259298c2df468f24a37ca4ebbca0e77057707c
K-Anant412 <anantkore412@gmail.com>
2026-08-09 10:36:32 +0530

    learnnig how to import UI components in Shadcn UI

--- my-shadcn-app/src/App.jsx
+++ my-shadcn-app/src/App.jsx
@@ -1,120 +1,122 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { Button } from '@/components/ui/button'
+
+import { FaGoogle } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa6";
+import { FaDiscord } from "react-icons/fa";
+
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
+      <section className='w-screen h-screen bg-linear-to-l from-[#007979] via-[#24B1B1] to-[#FFE2AF] flex items-center justify-center'>
+        {/* 
+        <div className="flex gap-4 p-10">
+          <Button>Default</Button>
+
+          <Button variant="secondary">
+            Secondary
+          </Button>
+
+          <Button variant="outline">
+            Outline
+          </Button>
+
+          <Button variant="ghost">
+            Ghost
+          </Button>
+
+          <Button variant="destructive">
+            Delete
+          </Button>
+        </div> 
+
+        <div className="flex items-center gap-4 p-10">
+          <Button size="sm">Small</Button>
+
+          <Button size="default">Default</Button>
+
+          <Button size="lg">Large</Button>
+
+          <Button size="icon">
+            +
+          </Button>
+        </div> */}
+
+        <Card  size="sm" className="mx-auto w-full max-w-xs pt-0 border-white shadow-[6px_8px_20px_rgba(0,0,0,0.22),-8px_-8px_20px_rgba(255,255,255,0.12)]">
+
+          <CardHeader className="w-full flex flex-col items-center bg-gray-400 text-white h-fit py-3">
+
+            <CardTitle className="text-[26px] font-semibold">
+              Login Card
+            </CardTitle>
+
+            <CardDescription className="font-bold">
+              login to start coding.
+            </CardDescription>
+
+          </CardHeader>
+
+          <CardContent>
+
+            <FieldGroup>
+              <Field>
+                <FieldLabel htmlFor="fieldgroup-name">Name</FieldLabel>
+                <Input id="fieldgroup-name" placeholder="new user" />
+              </Field>
+              <Field>
+                <FieldLabel htmlFor="fieldgroup-email">Email</FieldLabel>
+                    <Input
+                      id="fieldgroup-email"
+                      type="email"
+                      placeholder="name@example.com"
+                    />
+                <FieldDescription>
+                      start new journey towards a bright Future.
+                </FieldDescription>
+              </Field>
+                  <Field orientation="horizontal">
+                    <Button type="reset" variant="outline">
+                      Reset
+                    </Button>
+                    <Button type="submit">Submit</Button>
+                  </Field>
+            </FieldGroup>
+
+          </CardContent>
+
+          <CardFooter className="w-full h-fit py-3 items-start justify-between flex flex-col gap-2 bg-gray-400 text-white">
+            <p className='font-medium'>or Login with:</p>
+
+            <div className='w-full h-fit flex items-center gap-6 pl-[22%]'>
+              <FaGoogle size={24} />
+              <FaDiscord size={24} />
+              <FaLinkedin size={24} />
+              <FaGithub size={24} />
+            </div>
+          </CardFooter>
+
+        </Card>
       </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
     </>
   )
 }
